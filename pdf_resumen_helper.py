6a5bf69995af97d5b1b32c235eae9e66395196c5
gianmromero <gianmromerocastro@gmail.com>
2023-09-30 15:11:42 -0500

    no funciona

--- pdf_resumen_helper.py
+++ pdf_resumen_helper.py
@@ -4,14 +4,13 @@
 import fitz
 
 doc = fitz.open('sample.pdf')
-
 # Cantidad de páginas en el pdf.
 print(len(doc))
 # Tomando página para su posterior procesamiento.
 page = doc[0]
+
 # Lista para guardar las co-ordenadas de todas las palabras subrayadas.
 highlights = []
-
 # Itera hasta hallar una anotación de subrayación en la página
 annot = page.first_annot
 while annot:
@@ -26,7 +25,6 @@ while annot:
             for i in range(0, len(all_coordinates)):
                 coord = fitz.Quad(all_coordinates[i]).rect
                 highlights.append(coord)
-
         annot = annot.next 
 
 all_words = page.get_text()
